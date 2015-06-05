2cfd703016b21ef18777eaa48358e25b9ddccbeb
loucher <matus.svitek@gmail.com>
2015-06-05 15:01:53 +0200

    js cleanup

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,20 +1,15 @@
-//setup of clickable table rows
 jQuery(document).ready(function ($) {
     $(".clickable-row").click(function () {
         window.document.location = $(this).data("href");
     });
+    setNavigation();
 });
 
-//custom confirm function which will stop futher even propagation in case of nested onclick events
 function customConfirm(message) {
     var evt = window.event || arguments.callee.caller.arguments[0];
     evt.stopPropagation();
     return confirm(message);
 }
-
-$(function () {
-    setNavigation();
-});
 
 function setNavigation() {
     var path = window.location.pathname;
