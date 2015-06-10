ddf37e275c5894aa8404f60fe27e18c6a81dcfec
loucher <matus.svitek@gmail.com>
2015-06-10 23:58:29 +0200

    fixed wrappers

--- src/main/java/sk/stuba/fei/team/local/api/domain/FacilityWrapper.java
+++ src/main/java/sk/stuba/fei/team/local/api/domain/FacilityWrapper.java
@@ -1,16 +1,27 @@
 package sk.stuba.fei.team.local.api.domain;
 
+import sk.stuba.fei.team.local.domain.Facility;
+
 public class FacilityWrapper {
     private Long id;
     private String name;
     private String streetAndNumber;
     private String city;
     private String zip;
-    private String clientID;
-    private String clientSecret;
-    private String username;
-    private String password;
     private Boolean enabled;
+
+    public FacilityWrapper() {
+    }
+
+    public FacilityWrapper(Facility facility) {
+        id = facility.getId();
+        name = facility.getName();
+        streetAndNumber = facility.getStreetAndNumber();
+        city = facility.getCity();
+        zip = facility.getZip();
+        enabled = facility.getEnabled();
+    }
+
 
     public Long getId() {
         return id;
@@ -50,38 +61,6 @@ public class FacilityWrapper {
 
     public void setZip(String zip) {
         this.zip = zip;
-    }
-
-    public String getClientID() {
-        return clientID;
-    }
-
-    public void setClientID(String clientID) {
-        this.clientID = clientID;
-    }
-
-    public String getClientSecret() {
-        return clientSecret;
-    }
-
-    public void setClientSecret(String clientSecret) {
-        this.clientSecret = clientSecret;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public Boolean getEnabled() {
--- src/main/java/sk/stuba/fei/team/local/api/domain/SpecializationWrapper.java
+++ src/main/java/sk/stuba/fei/team/local/api/domain/SpecializationWrapper.java
@@ -1,9 +1,21 @@
 package sk.stuba.fei.team.local.api.domain;
 
+import sk.stuba.fei.team.local.domain.Specialization;
+
 public class SpecializationWrapper {
     private Long id;
     private String name;
     private Boolean enabled;
+
+    public SpecializationWrapper() {
+    }
+
+    public SpecializationWrapper(Specialization specialization) {
+        id = specialization.getId();
+        name = specialization.getName();
+        enabled = specialization.getEnabled();
+    }
+
 
     public Long getId() {
         return id;
