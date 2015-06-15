a8bc07dcc790ad7f39a5fcce674e89c487102dd3
Pavol Nemec <nemec.pavol@gmail.com>
2015-06-15 21:10:18 +0200

    OpeningHours - zmena String date na int date

--- src/main/java/sk/stuba/fei/team/local/domain/OpeningHours.java
+++ src/main/java/sk/stuba/fei/team/local/domain/OpeningHours.java
@@ -7,9 +7,9 @@ import java.io.Serializable;
 @Entity
 @XmlRootElement
 public class OpeningHours implements Serializable {
-    private static final long serialVersionUID = 7144402491802676227L;
-    private Long id;
-    private String date;
+    private static final long serialVersionUID = -6163945840447837521L;
+    private Long Id;
+    private int date;
     private Long reservationMorningFrom;
     private Long reservationMorningTo;
     private Long reservationFrom;
@@ -18,19 +18,19 @@ public class OpeningHours implements Serializable {
 
     @Id
     public Long getId() {
-        return id;
+        return Id;
     }
 
     public void setId(Long id) {
-        this.id = id;
+        Id = id;
     }
 
     @Column
-    public String getDate() {
+    public int getDate() {
         return date;
     }
 
-    public void setDate(String date) {
+    public void setDate(int date) {
         this.date = date;
     }
 
@@ -79,5 +79,4 @@ public class OpeningHours implements Serializable {
     public void setReservationMorningTo(Long reservationMorningTo) {
         this.reservationMorningTo = reservationMorningTo;
     }
-
 }
